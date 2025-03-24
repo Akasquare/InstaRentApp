0107cebb70c46e8356706efb0a30ca964d02cd3d
Aksquare <akashkush6272@gmail.com>
2025-03-24 12:37:18 +0530

    edit and update

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing= require("./models/listings.js");
 const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname , "views"));
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"));
+
+
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wandurlust";
 main()
@@ -40,12 +47,33 @@ app.get("/listings/:id", async (req, res)=>{
    res.render("listings/show.ejs", {listing})
 })
 
+//create route
 app.post("/listings", async (req, res)=>{
     // let {title, description , image, price ,country, location}= req.body;
     let newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 })
+//Edit route
+app.get("/listings/:id/edit", async (req, res)=>{
+    let {id}= req.params;
+    const listing= await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+//update route
+app.put("/listings/:id",async (req, res)=>{
+    let {id}= req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`)
+})
+//DELETE route
+app.delete("/listings/:id", async (req, res)=>{
+    let {id}= req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing)
+    res.redirect("/listings");
+})
+
 // app.get("/testListing", async (req, res)=>{
 //     let samplelisting = new Listing({
 //         title: "My new villa",
